1c83041d4383651bee716e25d0149a5bc9fae156
StanislauMaroz <36924642+StanislauMaroz@users.noreply.github.com>
2018-02-28 20:49:30 +0300

    add class person

---
+++ back/src/main/java/myProjects/ClassTask4.java
@@ -0,0 +1,26 @@
+package myProjects;
+
+/**
+ * Created by user on 28.02.2018.
+ */
+public class ClassTask4 {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+}
